df27de0fd4ab4bddf5010f1f0b96078399982c97
liujiaqi855 <55184894+liujiaqi855@users.noreply.github.com>
2019-10-13 15:00:48 +0800

    Add files via upload

---
+++ TexteditorTest.java
@@ -0,0 +1,20 @@
+package edit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TexteditorTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testTexteditor() {
+		
+	}
+
+
+}
